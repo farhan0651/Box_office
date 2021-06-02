277602c3b1eb18cf4f0a9e5d37f2306a60cddabb
farhan0651 <fk912254@gmail.com>
2021-06-02 14:09:15 +0530

    completed starred page

--- src/misc/Config.js
+++ src/misc/Config.js
@@ -1,3 +1,8 @@
+// fetch("https://chaturbate.com/api/public/affiliates/onlinerooms/?wm=3rays&gender=f&client_ip=request_ip").then((res)=>{
+// return(res.json());
+// }).then((res2)=>{console.log(res2.results[0])});
+
+
 const API_BASE_URL='https://api.tvmaze.com'
 
 export async function apiGET(queryString){
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,10 +1,44 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import MainPage from '../components/MainPage'
+import {useShows} from '../misc/custom-hooks'
+import {apiGET} from '../misc/Config'
+import ShowGrid from '../components/show/ShowGrid'
 
 const Starred = () => {
+
+    const [starred]=useShows();
+
+    const [shows,setShows]=useState(null);
+    const [isLoading,setIsLoading]=useState(true);
+    const [error,setError]=useState(null);
+
+    useEffect(()=>{
+        if(starred && starred.length>0){
+            const promises= starred.map(showId=> apiGET(`/shows/${showId}`));
+            Promise.all(promises).then(apiResult=>apiResult.map(show=>({show})))
+            .then(result=>{
+                setShows(result);
+                console.log(result);
+                setIsLoading(false);
+            })
+            .catch(err=>{
+                setError(err.message);
+                setIsLoading(false);
+            })
+        }
+        else{
+            setIsLoading(false);
+        }
+    },[starred])
+
     return (
-        <MainPage>This is Starred Page</MainPage>
+        <MainPage>
+            {shows && !isLoading && !error && <ShowGrid data={shows} />}
+            {isLoading && <div>The Shows are Loading</div>}
+            {error && <div>Error Occured:{error}</div>}
+            {!isLoading && !shows && <div>No starred show to display</div>}
+        </MainPage>
     )
 }
 
